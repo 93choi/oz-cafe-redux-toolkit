80c6af5646426bc10ad5c13c31bbee867246a704
93choi <cheonsa823@gmail.com>
2025-04-22 13:51:49 +0900

    oz코딩스쿨 0422 redux 과제 제출

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,18 +1,19 @@
 import { useState } from "react";
 import Item from "./Item";
 import OrderModal from "./OrderModal";
+import { useSelector } from "react-redux";
 
-function Menu({ menu, cart, setCart }) {
-  const [modalOn, setModalOn] = useState(false);
-  const [modalMenu, setModalMenu] = useState(null);
+function Menu() {
+  const menu = useSelector(state => state.menuReducer)
   if (!menu)
     return (
-      <div style={{ textAlign: "center", margin: "80px" }}>
+  <div style={{ textAlign: "center", margin: "80px" }}>
         {" "}
         메뉴 정보가 없어요!
       </div>
     );
-
+  const [modalOn, setModalOn] = useState(false);
+  const [modalMenu, setModalMenu] = useState(null);
   const categorys = Object.keys(menu);
   return (
     <>
@@ -39,8 +40,7 @@ function Menu({ menu, cart, setCart }) {
         <OrderModal
           modalMenu={modalMenu}
           setModalOn={setModalOn}
-          cart={cart}
-          setCart={setCart}
+
         />
       ) : null}
     </>
